d6411d02122b9509c41442a6671fb3f7edc3c35b
EUS-VICTORY <cenessence72@gmail.com>
2022-11-21 16:47:08 -0800

    test file

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - prints the character c to stdout
+ * @c: character to be printed
+ * Return: on success 1
+ * On error, returns -1, errno is set appropriately
+ */
+
+int _putchr(char c)
+{
+	return (write (1, &c, 1);
+}
